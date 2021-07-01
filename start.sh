2119082469539f511ae65ebf75eb95c03be36bb7
Hyunjae Kim <hyunjae.kim@gmx.com>
2021-07-01 16:35:07 +0900

    bashism fix

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 bundle update && bundle install
 ./generateData.js
 
-if [[ $1 = "docker" ]]; then
+if [ $1 = "docker" ]; then
     docker-compose up
-elif [[ $1 = "watch" ]]; then
+elif [ $1 = "watch" ]; then
     bundle exec jekyll server --watch
-elif [[ $1 = "inc" ]]; then
+elif [ $1 = "inc" ]; then
     bundle exec jekyll server --incremental
 fi
